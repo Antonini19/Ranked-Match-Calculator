d2e2316dcad5b9519329f16daeaebc4f042fda91
Antonini <leonardogodinho2014@gmail.com>
2025-02-08 00:38:42 -0300

    Segundo commit:faltou inclementar o loop

--- script.js
+++ script.js
@@ -2,8 +2,27 @@ function VitoriasEDerrotas(vitoria, derrotas) {
     return vitoria - derrotas;
 }
 
-     let resultadoRankeadas = VitoriasEDerrotas(10,5)
+let vitoria = parseInt(prompt("Digite seu saldo de vitorias:"))
+let derrotas = parseInt(prompt("Digite o seu numero de derrotas:"))
+let saldoVitorias = VitoriasEDerrotas(vitoria , derrotas)
 
-     console.log( `seu saldo de vitorias é:${resultadoRankeadas}`)
+
+    if (saldoVitorias < 10) {
+        ranke = "Ferro"
+    } else if ((saldoVitorias >11) &&(saldoVitorias<20)){
+        ranke = "Bronze" 
+    } else if ((saldoVitorias >21) && (saldoVitorias<50)){
+        ranke = "Prata"
+    } else if ((saldoVitorias > 51) && (saldoVitorias <80)){
+        ranke = "Ouro"
+    } else if ((saldoVitorias > 81) && (saldoVitorias < 90)){
+        ranke = "Diamante"
+    }else if ((saldoVitorias > 91) && (saldoVitorias < 100)){
+        ranke = "Lendário"
+    } else if (saldoVitorias>101){
+        ranke = "Imortal"
+    }
+
+    console.log(`O Herói tem o saldo de:${saldoVitorias}, e está no Ranke:${ranke}. `)
 
     
