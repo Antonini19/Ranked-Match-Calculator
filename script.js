6795def0e959600e14853f887f074bfe4d289bd2
Antonini <leonardogodinho2014@gmail.com>
2025-02-08 00:13:50 -0300

    primeiro passo

---
+++ script.js
@@ -0,0 +1,9 @@
+function VitoriasEDerrotas(vitoria, derrotas) {
+    return vitoria - derrotas;
+}
+
+     let resultadoRankeadas = VitoriasEDerrotas(10,5)
+
+     console.log( `seu saldo de vitorias é:${resultadoRankeadas}`)
+
+    
